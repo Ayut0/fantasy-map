cb577413cf2b523cf6a16f8cfe20c68a632fd6f5
Megumi Akama <kamayatu91@gmail.com>
2022-10-26 11:54:40 -0700

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
+import { Header } from "./components/Header";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#232946",
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: "#0066ff",
+      main: "#0044ff",
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: "#ffcc00",
+    },
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Header />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,75 @@
-import React from "react";
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import SearchIcon from "@mui/icons-material/Search";
+import { MdSearch } from "react-icons/md";
 
-export default function Header () {
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: "white",
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+
+export const Header: React.FC = () => {
   return (
-    <></>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h3" component="div" sx={{ display: "flex" }}>
+            Fantasy map
+          </Typography>
+          <Search>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+            <MdSearch />
+          </Search>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Register</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-}
+};

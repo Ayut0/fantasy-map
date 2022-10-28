cc06fb20cfdb85b359b07d027c9bb45bff59bcab
Megumi Akama <kamayatu91@gmail.com>
2022-10-28 16:02:45 -0700

    Add test event for search bar and buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,31 @@
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import InputBase from "@mui/material/InputBase";
 import { MdSearch } from "react-icons/md";
 import { Grid } from "@mui/material";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: "white",
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
+const handleInputChange = (
+  event: any
+) => {
+  console.log(event.target.value);
+};
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "black",
-  "& .MuiInputBase-input": {
-    // padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    // paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "12ch",
-      },
-    },
-  },
-}));
+const handleClickLogin = (event: any) => {
+  console.log("Login clicked");
+};
+
+const handleClickRegister = (event: any) => {
+  console.log("Register clicked");
+};
 
 export const Header: React.FC = () => {
   return (
     <Box>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ py: 1.5 }}>
         <Toolbar>
           <Grid container spacing={2}>
             <Grid
@@ -56,42 +37,51 @@ export const Header: React.FC = () => {
               }}
             >
               <Box component="div">
-                <Typography variant="h4" component="div">
+                <Typography variant="h4" component="div" sx={{ pr: 4 }}>
                   Fantasy map
                 </Typography>
               </Box>
               <Box component="div">
-                <Search
+                <TextField
+                  onChange={handleInputChange}
+                  id="outlined-basic"
+                  placeholder="Search"
+                  variant="outlined"
                   sx={{
-                    borderRadius: 5,
-                    height: 30,
+                    backgroundColor: "white",
+                    borderRadius: 8,
                   }}
-                >
-                  <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ "aria-label": "search" }}
-                  />
-                  <MdSearch
-                    style={{
-                      color: "#232946",
-                      paddingTop: 3,
-                      paddingRight: 3,
-                    }}
-                    size="30px"
-                  />
-                </Search>
+                  InputProps={{
+                    endAdornment: (
+                      <MdSearch
+                        style={{
+                          color: "#232946",
+                          paddingTop: 3,
+                          paddingRight: 3,
+                        }}
+                        size="40px"
+                      />
+                    ),
+                  }}
+                />
               </Box>
             </Grid>
-            <Grid item xs={6}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "right",
-                  alignItems: "flex-end",
-                }}
-              >
-                <Button color="inherit">Login</Button>
-                <Button color="inherit">Register</Button>
+            <Grid
+              item
+              xs={6}
+              sx={{
+                display: "flex",
+                justifyContent: "right",
+                alignItems: "flex-end",
+              }}
+            >
+              <Box>
+                <Button color="inherit" onClick={handleClickLogin}>
+                  Login
+                </Button>
+                <Button color="inherit" onClick={handleClickRegister}>
+                  Register
+                </Button>
               </Box>
             </Grid>
           </Grid>

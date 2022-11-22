52f31350504df955e6be095d2de75fa59dd4fae8
Megumi Akama <kamayatu91@gmail.com>
2022-11-22 13:58:41 -0800

    Finish fetch data in home page

--- src/pages/home/Buttons.tsx
+++ src/pages/home/Buttons.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import ActionButton from "../../components/ActionButton";
+import { Link } from "react-router-dom";
 
 export const Buttons: React.FC = () => {
-  return (<Button variant="contained" sx={
-    {backgroundColor: "#2CA58D",
-  borderRadius: "20px"}}>View List</Button>);
+  return (
+    <ActionButton
+      variant="contained"
+      sx={{
+        padding: ".5rem .8rem",
+        width: "65%",
+        fontSize: "1.1rem",
+        backgroundColor: "#2CA58D",
+        borderRadius: "50px"
+      }}
+    >
+      <Link style={{ textDecoration: "none", color: "#FFFFFF" }} to="/lists">
+        {/* Replace link to actual list later */}
+        View List
+      </Link>
+    </ActionButton>
+  );
 };
--- src/pages/home/PopularListCard.tsx
+++ src/pages/home/PopularListCard.tsx
@@ -1,108 +1,56 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {Avatar, Box, Card, CardContent, CardMedia, Grid, Typography} from "@mui/material";
 import { Buttons } from "./Buttons";
 
-interface PopularLists {
-  id: number;
-  name: string;
-  userId: number;
-  description: string;
-  picture: string;
-  userName: string;
-  userIcon: string;
-}
-
-const popularListsDummy: PopularLists[] = [
-  //delete this variable once real data available
-  {
-    id: 1,
-    name: "San diego",
-    userId: 3,
-    description: "Place you must visit in San diego",
-    picture: "https://kitsfest.com/wp-content/uploads/2013/05/brempong-sm.jpg",
-    userName: "Yuto Y",
-    userIcon: "https://",
-  },
-  {
-    id: 2,
-    name: "Wine place",
-    userId: 1,
-    description: "Fancy wine places only",
-    picture:
-      "https://winecountrytable.com/wp-content/uploads/2017/07/2017-6-14-Domaine-Carneros-Napa-Wineries-Wine-and-Cheese-Pairing-Blog-Size-0861.jpg",
-    userName: "Megumi-san",
-    userIcon: "https://",
-  },
-  {
-    id: 3,
-    name: "Brazilian restaurant",
-    userId: 5,
-    description: "Restaurant lists less than $30",
-    picture: "https://kitsfest.com/wp-content/uploads/2013/05/brempong-sm.jpg",
-    userName: "Mausan",
-    userIcon: "https://",
-  },
-  {
-    id: 4,
-    name: "Treats for dogs",
-    userId: 10,
-    description: "Zoey's favorite",
-    picture: "https://kitsfest.com/wp-content/uploads/2013/05/brempong-sm.jpg",
-    userName: "Arthur",
-    userIcon: "https://",
-  },
-  {
-    id: 5,
-    name: "Onakasuita",
-    userId: 2,
-    description: "Costco",
-    picture: "https://kitsfest.com/wp-content/uploads/2013/05/brempong-sm.jpg",
-    userName: "Nicolas Meza",
-    userIcon: "https://",
-  },
-  {
-    id: 6,
-    name: "Ramen",
-    userId: 4,
-    description: "Rich tonkotsu ramen in Vancouver",
-    picture: "https://kitsfest.com/wp-content/uploads/2013/05/brempong-sm.jpg",
-    userName: "Kento Honda",
-    userIcon: "https://",
-  },
-];
-
 export const PopularListCard: React.FC = () => {
+  const [popularLists, setPopularLists] = useState([]);
+  useEffect(() => {
+    axios.get("/api/lists/popular").then((res) => {
+      setPopularLists(res.data);
+    });
+  }, []);
+
   return (
     <Grid container rowSpacing={6} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-      {popularListsDummy.map((list) => {
+      {popularLists.map((list:any) => {
         return (
           <Grid key={list.id} item xs={4}>
-            <Card sx={{
-               width: "335px", 
-               height: "461px",
-               borderRadius: "4px" }}>
+            <Card
+              sx={{
+                width: "335px",
+                height: "461px",
+                borderRadius: "4px",
+              }}
+            >
               <CardContent>
-                <Typography variant="h4" component="div">
+                <Typography variant="h5" component="div" sx={{ pb: 1 }}>
                   {list.name}
                 </Typography>
                 <CardMedia
                   component="img"
                   height="194"
-                  image="https://images.unsplash.com/photo-1505245208761-ba872912fac0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+                  image={list.picture}
                   alt="List image"
                 />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {list.description}
-                </Typography>
+                <Box sx={{ p: 2, height: "50px" }}>
+                  <Typography color="text.secondary">
+                    {list.description}
+                  </Typography>
+                </Box>
                 <Buttons />
-                <br />
-                <img src="#" alt="profile icon" />
-                <Typography sx={{display: "inline"}}>{list.userName}</Typography>
+                <Grid container pt={3} sx={{ alignItems: "center" }}>
+                  <Grid item xs={1}></Grid>
+                  <Grid item xs={3}>
+                    <Avatar src={list.user.profilePicture} alt="profile icon" />
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography sx={{ display: "inline" }}>
+                      {list.user.name}
+                    </Typography>
+                  </Grid>
+                </Grid>
               </CardContent>
             </Card>
           </Grid>
